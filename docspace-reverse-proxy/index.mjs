abe6fdd42d046ae3d09cddaca27059b105c1e775
Sergey Malkin <sergey.malkin@onlyoffice.com>
2023-12-18 16:00:07 +0300

    Add logs

--- docspace-reverse-proxy/index.mjs
+++ docspace-reverse-proxy/index.mjs
@@ -97,16 +97,18 @@ export const handler = async (event, context, callback) => {
 
   // Redirect from DS to WS if match
   const workspaceRegex = /(\/products\/|\/addons\/|.aspx)/i;
-
+  
   if (workspaceRegex.test(request.uri))
   {
+      const newurl = `https://${tenantDomain.replace('onlyoffice.io', 'teamlab.info')}${request.uri}?${request.querystring}`;
+      console.log("redirect to: %s", newurl);
       const response = {
           status: '302',
           statusDescription: 'Found',
           headers: {
               location: [{
                   key: 'Location',
-                  value: `https://${tenantDomain.replace('onlyoffice.io', 'teamlab.info')}${request.uri}?${request.querystring}`,
+                  value: newurl,
               }],
           },
       };
