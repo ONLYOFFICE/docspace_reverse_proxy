8ba6a4880ad81a77b45f1e19ec7dc5a40084c2d1
Sergey Malkin <sergey.malkin@onlyoffice.com>
2023-12-18 14:14:45 +0300

    Add rewrite for test

--- docspace-reverse-proxy/index.mjs
+++ docspace-reverse-proxy/index.mjs
@@ -93,6 +93,27 @@ export const handler = async (event, context, callback) => {
 
   console.log("current tenant_domain from host header: %s", tenantDomain);
 
+
+
+  // Redirect from DS to WS if match
+  const workspaceRegex = /(\/products\/|\/addons\/|.aspx)/i;
+
+  if (workspaceRegex.test(request.uri))
+  {
+      const response = {
+          status: '302',
+          statusDescription: 'Found',
+          headers: {
+              location: [{
+                  key: 'Location',
+                  value: `https://${tenantDomain.replace('onlyoffice.io', 'teamlab.info')}${request.uri}?${request.querystring}`,
+              }],
+          },
+      };
+      
+      callback(null, response);
+  }
+
   let originDomain;
 
   if (request.uri == "/apisystem/portal/register" && request.method == "POST") {
