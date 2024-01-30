3e3d2969d665f57c0fa8d26fe43c5c3ed6865f87
Yaroslav Pshenichnikov <yaroslav.pshenichnikov@onlyoffice.com>
2024-01-30 18:29:22 +0300

    added logs

--- docspace-reverse-proxy/index.mjs
+++ docspace-reverse-proxy/index.mjs
@@ -37,11 +37,11 @@ async function getTenantRegion(ddbRegion, tenantDomain) {
     TableName: dynamodbTableName
   };
 
-  console.log("[DynamoDb] before send command get item %s with tenant domain %s", getItemParams, tenantDomain);
+  console.log("[DynamoDb] before send command get item %s with tenant domain %s", JSON.stringify(getItemParams), tenantDomain);
 
   const response = await ddbClient.send(new GetItemCommand(getItemParams));
 
-  console.log("[DynamoDb] responce send command get item %s with tenant domain %s", response, tenantDomain);
+  console.log("[DynamoDb] responce send command get item %s with tenant domain %s", JSON.stringify(response), tenantDomain);
 
   if (response && response.Item) {
 
@@ -73,7 +73,9 @@ export const handler = async (event, context, callback) => {
   let originDomain;
 
   if (request.uri.toLowerCase() == "/apisystem/portal/register" && request.method == "POST") {
-    console.log("this is body %s", request.body);
+    console.log("START: Register portal request");
+
+    console.log("Register portal request body %s", request.body);
 
     let body = JSON.parse(Buffer.from(request.body.data, 'base64').toString('utf8'));
     let regionFromRequest = body["awsRegion"];
@@ -111,6 +113,8 @@ export const handler = async (event, context, callback) => {
     }
 
     console.log("request after changed %s", JSON.stringify(request));
+
+    console.log("END: Register portal request");
 
     // Return to CloudFront
     return callback(null, request);
